10b0d43c3b2fba78fd584c8651098b9329e01ead
bojerg <bojerg@users.noreply.github.com>
2023-11-14 03:34:16 -0600

    Added conditional log in/log out based on auth state

--- frontend/pantryVision/src/app/component/user-menu/user-menu.component.ts
+++ frontend/pantryVision/src/app/component/user-menu/user-menu.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
+import { Subscription, switchMap, take } from 'rxjs';
 
 @Component({
   selector: 'app-user-menu',
@@ -8,9 +9,29 @@ import { AuthService } from '@auth0/auth0-angular';
 })
 export class UserMenuComponent {
   constructor(private auth: AuthService) {}
+  authenticated: boolean = false;
+  private authSub: Subscription;
 
+  ngOnInit(): void {
+    // The Auth0 SDK exposes an isAuthenticated$ observable on the AuthService class that allows you to check whether a user is authenticated or not.
+    // -- Surely there's a better way to manage user auth status, but for now here we are :)
+    this.authSub = this.auth.isAuthenticated$.pipe(take(1)).subscribe(value => this.authenticated = value);
+  }
+
+  ngOnDestroy(): void {
+    this.authSub.unsubscribe();
+  }
+  
   login() {
     this.auth.loginWithRedirect();
+  }
+
+  logout() {
+    this.auth.logout({ 
+      logoutParams: {
+        returnTo: document.location.origin 
+      }
+    });
   }
 
 }
