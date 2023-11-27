66b3b3833cccfbb940aafc8b5088c23198839126
bvanopd <bvanopd@gmail.com>
2023-11-27 14:57:32 -0600

    remove testing area from app. Fixes bug when saving an empty pantry.

--- frontend/pantryVision/src/app/service/user.service.ts
+++ frontend/pantryVision/src/app/service/user.service.ts
@@ -18,7 +18,11 @@ export class UserService {
     return this.httpClient.get<string>("/api/user/getPantry.do");
   }
   setUserPantry(pantry: string): void {
-    this.httpClient.put("/api/user/setPantry.do", pantry).subscribe(() => console.log("pantry saved"));
+    if (pantry === '') {
+      this.httpClient.put("/api/user/setPantry.do", []).subscribe(() => console.log("pantry saved"));
+    } else {
+      this.httpClient.put("/api/user/setPantry.do", pantry).subscribe(() => console.log("pantry saved"));
+    }
   }
   getPrivate(): Observable<string> {
     return this.httpClient.get<string>("/api/private");
