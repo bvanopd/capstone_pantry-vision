db7b2a3538d1e5a51e607cf8a1789b10455f1167
bvanopd <bvanopd@gmail.com>
2023-11-27 14:15:29 -0600

    [finishes #186532836] Show only first 12 ingredients in a group. Allow toggle to show more.

--- frontend/pantryVision/src/app/model/ingredientGroup.ts
+++ frontend/pantryVision/src/app/model/ingredientGroup.ts
@@ -3,9 +3,15 @@ import { Ingredient } from "./ingredient";
 export class IngredientGroup {
   description: string;
   ingredients: Ingredient[];
+  ingredientsToShow: number;
 
-  constructor(description: string, ingredients: Ingredient[]) {
+  constructor(description: string, ingredients: Ingredient[], ingredientsToShow: number) {
     this.description = description;
     this.ingredients = ingredients;
+    this.ingredientsToShow = ingredientsToShow
+  }
+
+  toggleIngredientsToShow() {
+    this.ingredientsToShow = this.ingredientsToShow === 12 ? this.ingredients.length : 12;
   }
 }
